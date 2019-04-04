4946f0615c9035e35d46c0c6ea50b87bb0092f89
Brianna McBurney <mcburneb@sheridancollege.ca>
2019-04-04 13:33:43 -0400

    Added CardComparator & started getting the game setup

---
+++ src/ca/sheridancollege/project/CardComparator.java
@@ -0,0 +1,66 @@
+package ca.sheridancollege.project;
+
+import java.util.Comparator;
+
+/**
+ *
+ * @author Brianna McBurney
+ */
+public class CardComparator implements Comparator<Card>{
+    
+    @Override
+    public int compare(Card card1, Card card2) {
+        int card1Value = 0;
+        switch (card1.getValue()) {
+            case SIX: card1Value = 6;
+                break;
+            case SEVEN: card1Value = 7;
+                break;
+            case EIGHT: card1Value = 8;
+                break;
+            case NINE: card1Value = 9;
+                break;
+            case TEN: card1Value = 10;
+                break;
+            case JACK: card1Value = 11;
+                break; 
+            case QUEEN: card1Value = 12;
+                break; 
+            case KING: card1Value = 13;
+                break;
+            case ACE: card1Value = 14;
+                break;
+        }
+        
+        int card2Value = 0;
+        switch (card2.getValue()) {
+            case SIX: card1Value = 6;
+                break;
+            case SEVEN: card1Value = 7;
+                break;
+            case EIGHT: card1Value = 8;
+                break;
+            case NINE: card1Value = 9;
+                break;
+            case TEN: card1Value = 10;
+                break;
+            case JACK: card1Value = 11;
+                break; 
+            case QUEEN: card1Value = 12;
+                break; 
+            case KING: card1Value = 13;
+                break;
+            case ACE: card1Value = 14;
+                break;
+        }
+        
+        int returnValue = 0;
+        if (card1Value > card2Value){
+            returnValue = 1;
+        } else if (card1Value < card2Value) {
+            returnValue = -1;
+        }
+        
+        return returnValue;
+    }
+}
--- src/ca/sheridancollege/project/Stupid.java
+++ src/ca/sheridancollege/project/Stupid.java
@@ -3,6 +3,7 @@ package ca.sheridancollege.project;
 import ca.sheridancollege.project.Card.Suit;
 import ca.sheridancollege.project.Card.Value;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 /**
@@ -10,6 +11,10 @@ import java.util.Scanner;
  * @author Brianna McBurney
  */
 public class Stupid {
+    private static ArrayList<Card> player1Hand;
+    private static ArrayList<Card> player2Hand;
+    private static ArrayList<Card> player3Hand;
+    private static Suit trump;
     /**
      * @param args the command line arguments
      */
@@ -28,9 +33,9 @@ public class Stupid {
         ArrayList<Card> remainingDeck = gameStart.deal(numPlayers, deck);
         
         // get players hands after the cards have been dealt
-        ArrayList<Card> player1Hand = gameStart.getPlayer1Hand();
-        ArrayList<Card> player2Hand = gameStart.getPlayer2Hand();
-        ArrayList<Card> player3Hand = gameStart.getPlayer3Hand();
+        player1Hand = gameStart.getPlayer1Hand();
+        player2Hand = gameStart.getPlayer2Hand();
+        player3Hand = gameStart.getPlayer3Hand();
         
         // creating players
         Player p1 = new Player("player 1", false, player1Hand);
@@ -38,11 +43,84 @@ public class Stupid {
         Player p3 = new Player("player 3", false, player3Hand);
         
         // create the trump suit based on the first card in the remainingDeck
-        Suit trump = gameStart.createTrump(remainingDeck);
+        trump = gameStart.createTrump(remainingDeck);
         
+        Player startPlayer = lowestTrumpValue(p1, p2, p3);
         
-        // ******** PROBLEM
-        // cant compare enums cause they are words not the actual values
+    }
+    
+    public static Player lowestTrumpValue(Player player1, Player player2, Player player3){
+        // make their card with the lowest value first
+        Collections.sort(player1Hand, new CardComparator());
+        Collections.sort(player2Hand, new CardComparator());
+        Collections.sort(player3Hand, new CardComparator());
         
+        Card p1Lowest = null;
+        for(int i=0;i<player1Hand.size();i++) {
+            Suit cardSuit = player1Hand.get(i).getSuit();
+            if (cardSuit == trump) {
+                p1Lowest = player1Hand.get(i);
+                break;
+            }
+        }
+        
+        Card p2Lowest = null;
+        for(int i=0;i<player2Hand.size();i++) {
+            Suit cardSuit = player2Hand.get(i).getSuit();
+            if (cardSuit == trump) {
+                p1Lowest = player2Hand.get(i);
+                break;
+            }
+        }
+        
+        Card p3Lowest = null;
+        for(int i=0;i<player3Hand.size();i++) {
+            Suit cardSuit = player3Hand.get(i).getSuit();
+            if (cardSuit == trump) {
+                p1Lowest = player3Hand.get(i);
+                break;
+            }
+        }
+        CardComparator cardComparator = new CardComparator();
+        int compare1n2 = cardComparator.compare(p1Lowest, p2Lowest);
+        
+        Player returnCard = null;
+        switch (compare1n2) {
+            case -1:
+                int compare1n3 = cardComparator.compare(p1Lowest, p3Lowest);
+                switch (compare1n3) {
+                    case -1:
+                        returnCard = player1;
+                    case 1:
+                        returnCard = player2;
+                        // p1 and p3 are equal
+                    default: returnCard = player3;
+                        break;
+                }   break;
+            case 1:
+                int compare2n3 = cardComparator.compare(p2Lowest, p3Lowest);
+                switch (compare2n3) {
+                    case -1:
+                        returnCard = player2;
+                    case 1:
+                        returnCard = player3;
+                        // p2 and p3 are equal
+                    default: returnCard = player3;
+                        break;
+                }   break;
+            default:// p1 and p2 are equals
+                int compare1or2n3 = cardComparator.compare(p2Lowest, p3Lowest);
+                switch (compare1or2n3) {
+                    case -1:
+                        returnCard = player2;
+                    case 1:
+                        returnCard = player3;
+                        // p2 and p3 are equal
+                    default: returnCard = player3;
+                        break;
+                }   break;
+        }
+        
+        return returnCard;
     }
 }
